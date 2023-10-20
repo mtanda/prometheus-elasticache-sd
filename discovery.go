e428b5b153fa7aac9fed1aaea603a99b26c991ed
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2023-10-20 18:43:11 +0900

    migrate to aws-sdk-go v2

--- discovery.go
+++ discovery.go
@@ -7,13 +7,14 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/elasticache"
-	"github.com/aws/aws-sdk-go/service/sts"
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
+	"github.com/aws/aws-sdk-go-v2/service/elasticache"
+	"github.com/aws/aws-sdk-go-v2/service/elasticache/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/discovery/targetgroup"
 	"github.com/prometheus/prometheus/util/strutil"
@@ -45,17 +46,16 @@ func newDiscovery(conf sdConfig, logger log.Logger) (*discovery, error) {
 		logger = log.NewNopLogger()
 	}
 
-	stsSvc := sts.New(session.New())
-	identity, err := stsSvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	ctx := context.TODO()
+	accountId, err := getAccountId(ctx)
 	if err != nil {
 		return nil, err
 	}
-	metadataSvc := ec2metadata.New(session.New())
 
 	var region string
 	for region == "" {
 		var err error
-		region, err = metadataSvc.Region()
+		region, err = getRegion(ctx)
 		if err != nil {
 			level.Error(logger).Log("msg", "could not get region", "err", err)
 			time.Sleep(time.Duration(5) * time.Second)
@@ -66,28 +66,70 @@ func newDiscovery(conf sdConfig, logger log.Logger) (*discovery, error) {
 	d := &discovery{
 		logger:          logger,
 		refreshInterval: conf.RefreshInterval,
-		accountID:       *identity.Account,
+		accountID:       accountId,
 		region:          region,
 	}
 
 	return d, nil
 }
 
+func getAccountId(ctx context.Context) (string, error) {
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRetryMaxAttempts(0))
+	if err != nil {
+		return "", err
+	}
+
+	client := sts.NewFromConfig(cfg)
+	response, err := client.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
+	if err != nil {
+		return "", err
+	}
+
+	return *response.Account, nil
+}
+
+func getRegion(ctx context.Context) (string, error) {
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRetryMaxAttempts(0))
+	if err != nil {
+		return "", err
+	}
+
+	client := imds.NewFromConfig(cfg)
+	response, err := client.GetRegion(ctx, &imds.GetRegionInput{})
+	if err != nil {
+		return "", err
+	}
+
+	return response.Region, nil
+}
+
 func (d *discovery) Run(ctx context.Context, ch chan<- []*targetgroup.Group) {
 	for c := time.Tick(time.Duration(d.refreshInterval) * time.Second); ; {
 		var tgs []*targetgroup.Group
 
-		sess := session.Must(session.NewSession())
-		client := elasticache.New(sess, &aws.Config{Region: aws.String(d.region)})
+		sdkConfig, err := config.LoadDefaultConfig(ctx, config.WithRegion(d.region))
+		if err != nil {
+			level.Error(d.logger).Log("msg", "could not load config", "err", err)
+			time.Sleep(time.Duration(d.refreshInterval) * time.Second)
+			continue
+		}
+		client := elasticache.NewFromConfig(sdkConfig)
 
 		input := &elasticache.DescribeCacheClustersInput{
 			ShowCacheNodeInfo: aws.Bool(true),
 		}
 
-		if err := client.DescribeCacheClustersPagesWithContext(ctx, input, func(out *elasticache.DescribeCacheClustersOutput, lastPage bool) bool {
+		paginator := elasticache.NewDescribeCacheClustersPaginator(client, input)
+		for paginator.HasMorePages() {
+			out, err := paginator.NextPage(ctx)
+			if err != nil {
+				level.Error(d.logger).Log("msg", "could not describe cache cluster", "err", err)
+				time.Sleep(time.Duration(d.refreshInterval) * time.Second)
+				continue
+			}
 			for _, cluster := range out.CacheClusters {
 				for _, node := range cluster.CacheNodes {
-					if node.Endpoint == nil || node.Endpoint.Address == nil || node.Endpoint.Port == nil {
+					if node.Endpoint.Address == nil {
 						continue // instance is not ready
 					}
 
@@ -100,23 +142,23 @@ func (d *discovery) Run(ctx context.Context, ch chan<- []*targetgroup.Group) {
 					labels[elasticacheLabelInstanceState] = model.LabelValue(*node.CacheNodeStatus)
 					labels[elasticacheLabelInstanceType] = model.LabelValue(*cluster.CacheNodeType)
 
-					addr := net.JoinHostPort(*node.Endpoint.Address, strconv.FormatInt(*node.Endpoint.Port, 10))
+					addr := net.JoinHostPort(*node.Endpoint.Address, strconv.FormatInt(int64(node.Endpoint.Port), 10))
 					labels[model.AddressLabel] = model.LabelValue(addr)
 
 					labels[elasticacheLabelEngine] = model.LabelValue(*cluster.Engine)
 					labels[elasticacheLabelEngineVersion] = model.LabelValue(*cluster.EngineVersion)
 
 					labels[elasticacheLabelEndpointAddress] = model.LabelValue(*node.Endpoint.Address)
-					labels[elasticacheLabelEndpointPort] = model.LabelValue(strconv.FormatInt(*node.Endpoint.Port, 10))
+					labels[elasticacheLabelEndpointPort] = model.LabelValue(strconv.FormatInt(int64(node.Endpoint.Port), 10))
 
-					tags, err := listTagsForInstance(client, d.accountID, d.region, cluster, node)
+					tags, err := listTagsForInstance(ctx, client, d.accountID, d.region, cluster, node)
 					if err != nil {
 						level.Error(d.logger).Log("msg", "could not list tags for elasticache instance", "err", err)
 						continue
 					}
 
 					for _, t := range tags.TagList {
-						if t == nil || t.Key == nil || t.Value == nil {
+						if t.Key == nil || t.Value == nil {
 							continue
 						}
 
@@ -131,11 +173,6 @@ func (d *discovery) Run(ctx context.Context, ch chan<- []*targetgroup.Group) {
 					})
 				}
 			}
-			return !lastPage
-		}); err != nil {
-			level.Error(d.logger).Log("msg", "could not describe elasticache instance", "err", err)
-			time.Sleep(time.Duration(d.refreshInterval) * time.Second)
-			continue
 		}
 
 		ch <- tgs
@@ -149,9 +186,9 @@ func (d *discovery) Run(ctx context.Context, ch chan<- []*targetgroup.Group) {
 	}
 }
 
-func listTagsForInstance(client *elasticache.ElastiCache, accountId string, region string, cluster *elasticache.CacheCluster, node *elasticache.CacheNode) (*elasticache.TagListMessage, error) {
+func listTagsForInstance(ctx context.Context, client *elasticache.Client, accountId string, region string, cluster types.CacheCluster, node types.CacheNode) (*elasticache.ListTagsForResourceOutput, error) {
 	input := &elasticache.ListTagsForResourceInput{
 		ResourceName: aws.String(fmt.Sprintf("arn:aws:elasticache:%s:%s:cluster:%s", region, accountId, *cluster.CacheClusterId)),
 	}
-	return client.ListTagsForResource(input)
+	return client.ListTagsForResource(ctx, input)
 }
